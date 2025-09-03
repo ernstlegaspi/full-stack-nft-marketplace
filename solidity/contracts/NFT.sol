a3541f496edf271092a1da33753de035b0860f1e
Akie <legaspiernstdanielle@gmail.com>
2025-09-03 11:44:34 +0800

    initial commit

---
+++ solidity/contracts/NFT.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    uint256 private tokenId;
+
+    constructor() ERC721("NFTMarketplace", "NFT") {}
+
+    function mintNFT(address _to, string memory _tokenURI) external {
+        _safeMint(_to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        tokenId++;
+    }
+
+    function burnNFT(uint256 _tokenId) external {
+        address owner = _requireOwned(_tokenId);
+        _checkAuthorized(owner, _msgSender(), _tokenId);
+        _burn(_tokenId);
+    }
+}
